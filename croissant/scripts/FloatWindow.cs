a167ac39c844b31453e9776bbc17110999827e34
darkrentin <darkrentin@gmail.com>
2025-02-27 16:05:05 +0800

    add debug button on FloatWindows for test

--- croissant/scripts/FloatWindow.cs
+++ croissant/scripts/FloatWindow.cs
@@ -3,9 +3,37 @@ using System;
 
 public partial class FloatWindow : Window
 {
+
+    public int speed = 10;
+
+    public Vector2I WindowSize;
+
+    [Export] public TextEdit textEdit;
     public override void _Ready()
     {
-        Borderless = true;
+        WindowSize = Size;
+    }
+
+    public override void _Process(double delta)
+    {
+        if(Input.IsKeyPressed(Key.Q))
+        {
+            Position = new Vector2I(Position.X - speed, Position.Y);
+        }
+        if(Input.IsKeyPressed(Key.D))
+        {
+            Position = new Vector2I(Position.X + speed, Position.Y);
+        }
+        if(Input.IsKeyPressed(Key.Z))
+        {
+            Position = new Vector2I(Position.X, Position.Y - speed);
+        }
+        if(Input.IsKeyPressed(Key.S))
+        {
+            Position = new Vector2I(Position.X, Position.Y + speed);
+        }
+
+        GD.Print(Position);
     }
 
     public void _on_button_pressed_B()
@@ -24,4 +52,16 @@ public partial class FloatWindow : Window
         Unresizable = !Unresizable;
         
     }
+
+    public void _on_button_pressed_A()
+    {
+        AlwaysOnTop = !AlwaysOnTop;
+    }
+
+    public void _on_button_pressed_S()
+    {
+        Size = WindowSize;
+    }
+
+
 }
