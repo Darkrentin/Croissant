017526d67467a256d8eb76fbc6d40d1a76235c17
Darkrentin <darkrentin@gmail.com>
2025-03-01 22:13:05 +0800

    Rewrite the FloatWIndow Class to add Draggable argument and remove the TestScene

--- croissant/GameManager.cs
+++ croissant/GameManager.cs
@@ -1,20 +1,33 @@
 using Godot;
+using GodotPlugins.Game;
 using System;
 
 public partial class GameManager : Node2D
 {
-    public static Vector2I ScreenSize;
 
     public static Window MainWindow;
+    public static Window FixWindow;
 
+    public static Vector2I ScreenSize;
     public override void _Ready()
     {
+        AddFixWindow();
         ScreenSize = DisplayServer.ScreenGetSize();
-        MainWindow = GetWindow();
     }
 
     public override void _Process(double delta)
     {
-        
+    }
+
+    public void AddFixWindow()
+    {
+        FixWindow = new Window();
+        FixWindow.Transparent = true;
+        FixWindow.TransparentBg = true;
+        FixWindow.AlwaysOnTop = true;
+        FixWindow.Size = new Vector2I(40, 40); //min size
+        FixWindow.Borderless = true;
+
+        AddChild(FixWindow);
     }
 }
---
+++ croissant/scripts/FloatWindow.cs
@@ -0,0 +1,76 @@
+using Godot;
+using System;
+
+public partial class FloatWindow : Window
+{
+	[Export] public bool Draggable = true;
+	[Export] public Vector2I TargetPosition;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		InitialPosition = WindowInitialPosition.CenterPrimaryScreen;
+		Position = new Vector2I(500, 500);
+		Size = new Vector2I(400, 400);
+		TargetPosition = Position;
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		BlockDrag();
+	}
+
+	private void BlockDrag()
+	{
+		if(!Draggable)
+		{
+			if(HasFocus())
+			{
+				GameManager.FixWindow.GrabFocus();
+			}
+		}
+	}
+
+	private bool SetWindowPosition(Vector2I newPosition)
+	{
+		int x = Position.X;
+		int y = Position.Y;
+		bool returnValue = true;
+
+		if(newPosition.X<0)
+		{
+			x = 0;
+			returnValue = false;
+		}
+		else if(newPosition.X > GameManager.ScreenSize.X-Size.X)
+		{
+			x = GameManager.ScreenSize.X-Size.X;
+			returnValue = false;
+		}
+		else
+		{
+			x = newPosition.X;
+		}
+
+		if(newPosition.Y<0)
+		{
+			y = 0;
+			returnValue = false;
+		}
+		else if(newPosition.Y > GameManager.ScreenSize.Y-Size.Y)
+		{
+			y = GameManager.ScreenSize.Y-Size.Y;
+			returnValue = false;
+		}
+		else
+		{
+			y = newPosition.Y;
+		}
+
+		Position = new Vector2I(x, y);
+
+		return returnValue;
+	}
+}
