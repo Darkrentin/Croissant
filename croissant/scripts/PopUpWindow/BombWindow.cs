147d476581610e28167368981945c0d36f55e6d8
Adrien <adrien.coureau91@gmail.com>
2025-03-20 01:29:48 +0800

    BombWindow 4 new png assets

--- croissant/scripts/PopUpWindow/BombWindow.cs
+++ croissant/scripts/PopUpWindow/BombWindow.cs
@@ -7,23 +7,40 @@ public partial class BombWindow : PopUpWindow
 	[Export] public Timer timer;
 	[Export] public ProgressBar progressBar;
 	[Export] AnimatedSprite2D Sprite;
+	[Export] TextureRect Texture;
+
 
 	private float time;
 	public override void _Ready()
 	{
-		int randNum = Lib.rand.Next(0, 9);
-		if (randNum == 8)
+		int randNum = Lib.rand.Next(0, 4);
+		switch (randNum)
 		{
-			Sprite.Visible = true;
+			case 0:
+				Texture.Texture = ResourceLoader.Load<Godot.Texture2D>("res://assets/popup/bomb1.png");
+				break;
+			case 1:
+				Texture.Texture = ResourceLoader.Load<Godot.Texture2D>("res://assets/popup/bomb2.png");
+				break;
+			case 2:
+				Sprite.Visible = true;
+				Sprite.Animation = "banana";
+				Sprite.Play();
+				break;
+			case 3:
+				Sprite.Visible = true;
+				Sprite.Animation = "spin";
+				Sprite.Play();
+				break;
 		}
 		base._Ready();
 		Parent = GetParent<Level1>();
-		time = Lib.rand.Next(8, 10);
+		time = 8f;
 		progressBar.MaxValue = time * 100f;
 		timer.WaitTime = time;
 		Size = Lib.GetScreenSize(Lib.GetPercentage(new Vector2I(250, 250)));
 		SetWindowPosition(Lib.GetScreenPosition(Lib.GetRandomNormal(0f, 0.90f), Lib.GetRandomNormal(0f, 0.90f)));
-		progressBar.Size = Size;
+		progressBar.SetDeferred("size", Size);
 		timer.Start();
 
 		Title = "💣";
