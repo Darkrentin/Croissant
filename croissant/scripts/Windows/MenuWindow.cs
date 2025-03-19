b89fd1e10d77895324bc6a7cac2c0bc8299276d6
Adrien <adrien.coureau91@gmail.com>
2025-03-19 18:42:52 +0800

    fix deffered error in editor

--- croissant/scripts/Windows/MenuWindow.cs
+++ croissant/scripts/Windows/MenuWindow.cs
@@ -4,22 +4,22 @@ using System;
 public partial class MenuWindow : FloatWindow
 {
 	[Export] private Control Menu;
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
-		Size = Lib.GetScreenSize(0.1f, 0.2f);
-		Position = Lib.GetScreenPosition(0.5f, 0.5f) - Size / 2;
-		Visible = false;
-		Menu.Size = Size;
 
+		Vector2I windowSize = Lib.GetScreenSize(0.1f, 0.2f);
+		SetDeferred("size", windowSize);
+		Position = Lib.GetScreenPosition(0.5f, 0.5f) - windowSize / 2;
+		Visible = false;
+
+		SetDeferred("custom_minimum_size", windowSize);
+		Menu.SetDeferred("size", windowSize);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-
 
 		if (Input.IsActionJustPressed("Exit"))
 		{
