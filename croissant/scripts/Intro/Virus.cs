e626c8bcd365b8265a12ed676119893c13dae92f
Adrien <adrien.coureau91@gmail.com>
2025-03-19 16:01:30 +0800

    glitch shader added, virus is now a computer with a vhs shader

--- croissant/scripts/Intro/Virus.cs
+++ croissant/scripts/Intro/Virus.cs
@@ -4,22 +4,22 @@ using System;
 public partial class Virus : FloatWindow
 {
 	[Export] Camera3D Camera;
-	[Export] MeshInstance3D Mesh;
+	[Export] Node3D Computer;
 
 	Vector2I screenSize = DisplayServer.ScreenGetSize();
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
 		Position = Lib.GetScreenPosition(0.25f, 0.25f);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-		Mesh.RotateY(3 * (float)delta);
-		StartExponentialTransition(Lib.GetScreenPosition(Lib.GetRandomNormal(0, 1), Lib.GetRandomNormal(0, 1)), 1f);
+		Computer.RotateY(1 * (float)delta);
+
+		//Movement Fiesta
+		//StartExponentialTransition(Lib.GetScreenPosition(Lib.GetRandomNormal(0, 1), Lib.GetRandomNormal(0, 1)), 1f);
 	}
 }
