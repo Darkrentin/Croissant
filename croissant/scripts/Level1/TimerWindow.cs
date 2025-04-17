a1d35d469263e154f76b1308d80578fbb2bc27f5
Adrien <adrien.coureau91@gmail.com>
2025-04-17 11:11:33 +0800

    modified project settings and little cleanup

--- croissant/scripts/Level1/TimerWindow.cs
+++ croissant/scripts/Level1/TimerWindow.cs
@@ -17,26 +17,20 @@ public partial class TimerWindow : PopUpWindow
         progressBar.MaxValue = time * 100f;
         timer.WaitTime = time;
         timer.Timeout += _on_timer_timeout;
-        Size = (Vector2I)Lib.GetAspectFactor(new Vector2I(400, 600));
+        Size = Lib.GetAspectFactor(new Vector2I(400, 600));
         progressBar.Size = Size;
         timer.Start();
     }
 
-        // more there is windows easier is it to close them
+    // more there is windows easier is it to close them
     private float CalculateTimerDuration()
     {
-        if (Level1.WindowCount >= 10)
-        {
-            return Lib.rand.Next(9, 11);
-        }
-        else if (Level1.WindowCount >= 5)
-        {
-            return Lib.rand.Next(5, 7);
-        }
+        if (Level1.WindowCount >= 15)
+            return Lib.rand.Next(8, 10);
+        else if (Level1.WindowCount >= 10)
+            return Lib.rand.Next(6, 8);
         else
-        {
-            return Lib.rand.Next(3, 5);
-        }
+            return Lib.rand.Next(4, 6);
     }
 
     public override void OnClose()
@@ -55,9 +49,7 @@ public partial class TimerWindow : PopUpWindow
     {
         Level1.WindowCount--;
         for (int i = 0; i < 2; i++)
-        {
             Level1.AddNewWindow();
-        }
         QueueFree();
     }
 
