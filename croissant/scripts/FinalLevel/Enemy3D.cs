342ef5884de0192f24771e04ec2f96b30e333e6b
Adrien <adrien.coureau91@gmail.com>
2025-05-07 22:54:51 +0800

    added enemy shapes, models, bullet

--- croissant/scripts/FinalLevel/Enemy3D.cs
+++ croissant/scripts/FinalLevel/Enemy3D.cs
@@ -1,20 +1,62 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
-public partial class Enemy3D : Node3D
+public partial class Enemy3D : Area3D
 {
 	[Export] private MeshInstance3D Mesh;
-	private Vector3 RotationAxis; // Axe de rotation (Y)
+	[Export] private Mesh IcosahedronMesh;
+	[Export] private Mesh DodecahedronMesh;
+	[Export] private Mesh CubeMesh;
+	[Export] private Mesh TetrahedronMesh;
+	[Export] private AnimationPlayer AnimationPlayer;
+	[Export] private CollisionShape3D Collision;
+	private Vector3 RotationAxis;
+	private int currentShapeIndex;
+	private List<Mesh> shapeSequence;
+	public bool Alive = true;
+	private float rotationSpeed = 2.0f;
+	private int totalShapes = 4;
 
 	public override void _Ready()
 	{
 		RotationAxis = new Vector3(Lib.GetRandomNormal(-1, 1), Lib.GetRandomNormal(-1, 1), Lib.GetRandomNormal(-1, 1));
+		shapeSequence = new List<Mesh> { IcosahedronMesh, DodecahedronMesh, CubeMesh, TetrahedronMesh };
+		currentShapeIndex = Lib.rand.Next(0, totalShapes);
+		UpdateShape();
 	}
-
 
 	public override void _Process(double delta)
 	{
-		Rotation += RotationAxis * (float)delta * 2f;
+		Rotation += RotationAxis * (float)delta * rotationSpeed;
 		Mesh.Rotation = Rotation;
+	}
+
+	private void UpdateShape()
+	{
+		Mesh.Mesh = shapeSequence[currentShapeIndex];
+	}
+
+	public void OnBulletCollide()
+	{
+		if (!Alive) return;
+		currentShapeIndex++;
+		if (currentShapeIndex >= totalShapes)
+			Destroy();
+		else
+			UpdateShape();
+	}
+
+	private void Destroy()
+	{
+		Alive = false;
+		Collision.Disabled = true;
+		AnimationPlayer.Play("Depop");
+		Mesh.Visible = false;
+		Timer destroyTimer = new Timer();
+		destroyTimer.WaitTime = 0.5f;
+		destroyTimer.OneShot = true;
+		AddChild(destroyTimer);
+		destroyTimer.Timeout += () => QueueFree();
+		destroyTimer.Start();
 	}
 }
--- croissant/scripts/FinalLevel/Player3D.cs
+++ croissant/scripts/FinalLevel/Player3D.cs
@@ -3,28 +3,63 @@ using System;
 
 public partial class Player3D : CharacterBody3D
 {
-	// Called when the node enters the scene tree for the first time.
-	[Export] public float MoveSpeed = 10.0f;
-	[Export] public float RotationSpeed = 3.0f;
+	[Export] private AnimationTree AnimationTree;
+	[Export] private Node3D Bullet3DSpawnPosition;
+	[Export] private PackedScene Bullet3DScene;
+	[Export] private PackedScene Enemy3DScene;
+	[Export] private float MoveSpeed = 6.0f;
+	[Export] private float RotationSpeed = 4.0f;
+	public AnimationNodeStateMachinePlayback AnimationPlayer;
+	private Timer ShootTimer = new Timer();
+	private bool CanShoot = true;
+
 	public override void _Ready()
 	{
+		AnimationPlayer = (AnimationNodeStateMachinePlayback)(AnimationTree.Get("parameters/playback"));
+		ShootTimer.Timeout += () => CanShoot = true;
+		ShootTimer.WaitTime = 0.95f;
+		ShootTimer.OneShot = true;
+		AddChild(ShootTimer);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (Input.IsActionPressed("Shoot"))
+		{
+			if (CanShoot)
+			{
+				CanShoot = false;
+				Shoot(delta);
+				ShootTimer.Start();
+			}
+		}
 	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        float z_movement = Input.GetActionStrength("Backward") - Input.GetActionStrength("Forward");
+	public override void _PhysicsProcess(double delta)
+	{
+		float z_movement = Input.GetActionStrength("Backward") - Input.GetActionStrength("Forward");
 		float rotate = Input.GetActionStrength("LeftRot") - Input.GetActionStrength("RightRot");
 
-		Rotation = new Vector3(Rotation.X,Rotation.Y + RotationSpeed * rotate * (float)delta, Rotation.Z);
+		Rotation = new Vector3(Rotation.X, Rotation.Y + RotationSpeed * rotate * (float)delta, Rotation.Z);
 		Vector3 direction = new Vector3(0, 0, 1).Rotated(new Vector3(0, 1, 0), Rotation.Y);
 		Vector3 motion = z_movement * direction * MoveSpeed * (float)delta;
 
 		MoveAndCollide(motion);
-    }
 
+		if (motion.LengthSquared() > 0)
+			AnimationPlayer.Travel("Run");
+		else
+			AnimationPlayer.Travel("RESET");
+	}
+
+	private void Shoot(double delta)
+	{
+		AnimationPlayer.Travel("Shoot");
+
+		Bullet3D Bullet3D = Bullet3DScene.Instantiate<Bullet3D>();
+		Bullet3D.Position = Bullet3DSpawnPosition.GlobalPosition;
+		Bullet3D.Rotation = Rotation;
+		Bullet3D.Velocity = (Bullet3DSpawnPosition.GlobalPosition - (GlobalPosition + new Vector3(0, Scale.Y, 0))).Normalized() * 3000 * (float)delta;
+		GetParent().AddChild(Bullet3D);
+	}
 }
