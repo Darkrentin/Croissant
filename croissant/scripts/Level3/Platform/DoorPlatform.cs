0ae4cb0fdc19b26349c6447501e9ce681350b48c
Adrien <adrien.coureau91@gmail.com>
2025-05-22 19:28:55 +0800

    typo fix : replaced every "Plateform" with "Platform"

--- croissant/scripts/Level3/Platform/DoorPlatform.cs
+++ croissant/scripts/Level3/Platform/DoorPlatform.cs
@@ -1,11 +1,10 @@
-using System.Drawing;
 using Godot;
 
-public partial class DoorPlateform : Platform
+public partial class DoorPlatform : Platform
 {
-	[Export] public Label label;
-	public bool isOpen = false;
-	[Export] public int nbOfFilesToOpen = 1;
+    [Export] public Label label;
+    public bool isOpen = false;
+    [Export] public int nbOfFilesToOpen = 1;
     public override void _Ready()
     {
         base._Ready();
@@ -21,12 +20,11 @@ public partial class DoorPlateform : Platform
     public override void _Process(double delta)
     {
         base._Process(delta);
-		label.Text = $"{Level3.Instance.FilesCollected}/{nbOfFilesToOpen}";
-		if(Level3.Instance.FilesCollected >= nbOfFilesToOpen)
-		{
-			Position = GameManager.ScreenSize*3;
+        label.Text = $"{Level3.Instance.FilesCollected}/{nbOfFilesToOpen}";
+        if (Level3.Instance.FilesCollected >= nbOfFilesToOpen)
+        {
+            Position = GameManager.ScreenSize * 3;
             window.Position = (Vector2I)Position;
-		}
+        }
     }
 }
-    
